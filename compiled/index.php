415b12a6925757d3d8a33ee7b7af445ad731448c
SinghDominik <106447873+SinghDominik@users.noreply.github.com>
2022-07-06 07:24:07 +0200

    Add Feedback-Form

--- compiled/index.php
+++ compiled/index.php
@@ -1,14 +1,30 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/main.css" rel="stylesheet">
-    <title>TITLE</title>
-</head>
-<body>    <div id="content">
-        <?php require_once(PATH . "/templates/menue.tpl"); ?>
-        
-    </div></body>
+	<head>
+		<title></title>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<link rel="stylesheet" href="./css/main.css">
+	</head>
+	<body>
+		        <div id="content">
+			<strong>Willkommen im Feedback-Tool von Fridays For Future Rhein-Sieg.</strong><br />
+            Du kannst den Delis von Rhein-Sieg hiermit Feedback mitteilen, egal, ob negativ oder positiv.<br />
+            Wichtig dabei: Jedes Feedback muss konstruktiv sein. Das bedeutet:<br />
+            <ul>
+                <li>Es werden keine Beleidigungen oder sonstige respektlose Äußerungen benutzt. Feedback mit Beleidigungen wird konsequent aussortiert, ohne sich damit zu beschäftigen.</li>
+                <li>Es sollte, wenn möglich, eine mögliche Lösungsoption mitgegeben werden. Z.B.: "Die Plena sind viel zu häufig. Mein Vorschlag ist es, sie nur zweiwöchentlich zu veranstalten."</li>
+
+            </ul>
+			<div id="feedback-form">
+				<form action="index.php" method="POST">
+					<input type="text" name="name" id="name" placeholder="Name (optional)" multiple /><br />
+					<textarea name="feedback" id="feedback" placeholder="Dein Feedback"></textarea>
+					<input type="submit" value="Absenden" />
+				</form>
+			</div>
+		</div>
+	    </body>
 </html>
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,4 +1,18 @@
-    <div id="content">
-        <?php require_once(PATH . "/templates/menue.tpl"); ?>
-        
-    </div>
+        <div id="content">
+			<strong>Willkommen im Feedback-Tool von Fridays For Future Rhein-Sieg.</strong><br />
+            Du kannst den Delis von Rhein-Sieg hiermit Feedback mitteilen, egal, ob negativ oder positiv.<br />
+            Wichtig dabei: Jedes Feedback muss konstruktiv sein. Das bedeutet:<br />
+            <ul>
+                <li>Es werden keine Beleidigungen oder sonstige respektlose Äußerungen benutzt. Feedback mit Beleidigungen wird konsequent aussortiert, ohne sich damit zu beschäftigen.</li>
+                <li>Es sollte, wenn möglich, eine mögliche Lösungsoption mitgegeben werden. Z.B.: "Die Plena sind viel zu häufig. Mein Vorschlag ist es, sie nur zweiwöchentlich zu veranstalten."</li>
+
+            </ul>
+			<div id="feedback-form">
+				<form action="index.php" method="POST">
+					<input type="text" name="name" id="name" placeholder="Name (optional)" multiple /><br />
+					<textarea name="feedback" id="feedback" placeholder="Dein Feedback"></textarea>
+					<input type="submit" value="Absenden" />
+				</form>
+			</div>
+		</div>
+	
